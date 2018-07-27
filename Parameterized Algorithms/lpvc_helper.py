e6cf62b74340be03957f96836db2930ca502ddbc
Amir Arbel <amirarbel@gmail.com>
2018-07-27 14:28:17 +0300

    Add command line arguments

--- Parameterized Algorithms/lpvc_helper.py
+++ Parameterized Algorithms/lpvc_helper.py
@@ -13,28 +13,47 @@ def _is_all_half(x_v):
     return True;
 
 # Lemma 3.7.
-def get_optimum_lpvc_solution(graph, benchmark, debug = 0):
+def get_optimum_lpvc_solution(graph, benchmark, k, debug = 0):
     possible_half_integral = get_lp_kernel(graph, benchmark debug);
     possible_x_v = [xv for v,xv in possible_half_integral];
     # Optimum half-integral solution baseline "k";
     if not (_is_all_half(possible_x_v)):
         # Not all-1/2 solution, "good enough".
-        return {"k": sum(possible_x_v), "X": possible_half_integral};
+        return {"k": k, "kernel": possible_half_integral, "removed": []};
     # All 1/2 solution. For all v in V(G), check kernel for G-v.
     vc_star_g = sum(x_v);
     for node in graph.nodes():
+        # Work on a sub-graph without one of the vertices.
         graph.copy();
         graph.remove_node(node);
+
+        # Find the kernel as if we chose the node to be in the vertex.
         half_integral = get_lp_kernel(graph, benchmark debug);
+        # Have the node inside the kernel.
+        half_integral.append((node, 1));
+
         x_v = [xv for v,xv in half_integral];
         if sum(x_v) <= vc_star_g - 1:
             # Found an optimum solution.
-            return {"k": sum(x_v), "X": half_integral};
+            return {"k": k - 1, "kernel": half_integral, "removed": [node]};
     # Failed - the 1/2 solution is the optimum solution for LPVC(G).
-    return {"k": sum(possible_x_v), "X": possible_half_integral};
+    return {"k": k, "kernel": possible_half_integral, "removed": []}};
+
+def exhaust_vc_5(graph, benchmark, k, debug = 0):
+    k = 0;
+    cover = [];
+
+    # Apply lemma 3.7
+    optimum = get_optimum_lpvc_solution(graph, benchmark, debug);
+    if not optimum["removed"]:
+        # Empty "removed". Either all half or one removed.
+        if (_is_all_half([xv for v, xv in optimum["kernel"]])):
+            # Exhausted reduction vc 5.
+            return optimum;
+        # We have at least one new item in cover,
 
 
-
+# Reduction VC 4 removes all vertices in V_0 and V_1, and puts all V_1 nodes in cover.
 def reduction_vc_4(graph, kernel, k):
     nodes = [];
     cover = [];
@@ -63,5 +82,6 @@ def reduction_vc_4(graph, kernel, k):
     return {
         "success": True,
         "graph": new_graph,
-        "cover": cover
+        "cover": cover,
+        "k": k - len(cover)
     }
--- Parameterized Algorithms/vc.py
+++ Parameterized Algorithms/vc.py
@@ -8,20 +8,13 @@ from reductions import *
 from vc_kernel import *
 from branching import *
 from benchmark import *
-
-debug = 0;
-graph_name = "1000_0.1.gml";
-# graph_name = "test_graph.gml"
+import argparse
 
 def destringizer(str):
     return int(str);
 
-def vc(graph_name):
+def vc(graph, benchmark, k, debug):
 
-    # Load graph.
-    # graph = gml.read_gml(graph_name, "label", destringizer);
-    graph = nx.star_graph(1000);
-    benchmark.add("loading");
     # Run the included approximation algoritm for comparison.
     approx_cover = algorithms.approximation.vertex_cover.min_weighted_vertex_cover(graph);
     benchmark.add("Networkx vertex cover approximation algorithm: " + str(len(approx_cover)) + " nodes in cover.");
@@ -30,11 +23,13 @@ def vc(graph_name):
     kernel = get_lp_kernel(graph, benchmark, debug);
     benchmark.add("kernel found");
     cleaned_kernel = [xv for v, xv in kernel];
-    k = sum(cleaned_kernel);
+    if (k == -1):
+        # Unspecified k variable.
+        # TODO: Can we reduce from 2 * k?
+        k = 2 * sum(cleaned_kernel);
 
     # Use the kernel: branching.
-    # TODO: Can we reduce from 2 * k?
-    cover = vertex_cover_from_kernel(graph, kernel, 2 * k, debug);
+    cover = vertex_cover_from_kernel(graph, kernel, k, debug);
     benchmark.add("vertex cover from kernel");
 
     print "Calculated", len(cover), "vertex cover: ", cover;
@@ -64,6 +59,32 @@ def show_result(graph, cover):
     nx.draw(graph, pos=pos, node_color=node_colors, edge_color=edge_colors, with_labels=True);
     plt.show();
 
-# FIXME Temp; remove
-benchmark = benchmarker(graph_name);
-vc(graph_name);
+def start_vc(file_name, k = -1, debug = 0):
+    benchmark = benchmarker(file_name);
+
+    # Load graph.
+    graph = gml.read_gml(file_name, "label", destringizer);
+    benchmark.add("loading");
+    vc(graph = graph, benchmark=benchmark, debug=debug, k=k);
+
+
+def start_lpvc(file_name, debug = 0):
+    # Todo
+    pass
+
+parser = argparse.ArgumentParser(description='Start vertex cover algorithm.');
+parser.add_argument('file',
+                    help=".gml file to process.",
+                    type = str, default = "test_graph.gml");
+parser.add_argument('--k',
+                    help="Limit vertex cover by k.",
+                    type = int, default = "-1");
+parser.add_argument('--debug',
+                    help="Debug level integer. 0 For none, 1 for messages, 2 for graphs.",
+                    type = int, default = 0);
+parser.add_argument('--lpvc', dest='start', action="store_const",
+                   const = start_lpvc, default = start_vc,
+                   help = 'Use lpvc algorithm (Theorem 3.4).');
+
+args = parser.parse_args();
+args.start(file_name=args.file, debug=args.debug, k=args.k);
